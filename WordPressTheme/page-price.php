577cca2084614e723397d47e5ffa5cdeb81de378
yuka <yuka.68cn@gmail.com>
2025-10-14 03:37:35 +0900

    price

---
+++ WordPressTheme/page-price.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+<!-- ページコンテンツ -->
+
+<div class="page-price page-price-layout">
+    <div class="page-price__body price-tables">
+        <div class="price-tables__inner inner">
+
+        <?php
+         $tables = [
+            'ライセンス講習' => 'license',
+            '体験ダイビング' => 'trial', 
+            'ファンダイビング' => 'fun', 
+            'スペシャルダイビング' => 'special',
+         ];
+
+         foreach ($tables as $title => $field_name):
+          $courses = SCF::get($field_name);
+          if (!empty($courses)): ?>
+
+          <div class="price-tables__item price-table">
+            <h2 class="price-table__heading">
+              <span class="price-table__icon"></span>
+              <span class="price-table__title"><?php echo esc_html($title); ?></span>
+            </h2>
+            <table class="price-table__body">
+              <tbody>
+              <?php foreach ($courses as $course): ?>
+                <tr>
+                  <th scope="row"><?php echo nl2br(esc_html($course[$field_name . '_course'])); ?></th>
+                  <td><?php echo esc_html($course[$field_name . '_price']); ?></td>
+                </tr>
+              <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
+        <?php endif; endforeach; ?>
+
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
