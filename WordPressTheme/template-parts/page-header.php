15afb5aaa75071e34ef8edc15e71cebc89e3a292
yuka <yuka.68cn@gmail.com>
2025-10-17 03:47:49 +0900

    page-header修正

--- WordPressTheme/template-parts/page-header.php
+++ WordPressTheme/template-parts/page-header.php
@@ -1,23 +1,89 @@
 
-  <!-- ページタイトル -->
-  <?php
-  $page_header_sp = get_field('page_header_sp');
-  $page_header_pc = get_field('page_header_pc');
-  ?>
+<!-- ページタイトル -->
+<?php
+$post_type = get_post_type();
+$template = get_page_template_slug();
+$page_id  = get_the_ID();
+$slug     = get_post_field('post_name', $page_id);
 
-  <div class="page-header">
-    <h1 class="page-header__title"><?php the_title(); ?></h1>
-    <div class="page-header__image">
+// ----------------------
+// デフォルト
+$header_pc = get_theme_file_uri('/assets/images/pages/blog-fv-pc.jpg');
+$header_sp = get_theme_file_uri('/assets/images/pages/blog-fv-sp.jpg');
+$header_title = get_the_title();
+
+// ----------------------
+// blogページ
+if ($template === 'home.php' || $post_type === 'post') {
+    $header_title = 'Blog';
+
+// ----------------------
+// campaignページ
+} elseif (is_post_type_archive('campaign')){
+  $header_pc = get_theme_file_uri('/assets/images/pages/campaign-fv-pc.jpg');
+  $header_sp = get_theme_file_uri('/assets/images/pages/campaign-fv-sp.jpg');
+  $header_title = 'Campaign';
+
+// ----------------------
+// voiceページ
+} elseif (is_post_type_archive('voice')) {
+  $header_pc = get_theme_file_uri('/assets/images/pages/voice-fv-pc.jpg');
+  $header_sp = get_theme_file_uri('/assets/images/pages/voice-fv-sp.jpg');
+  $header_title = 'Voice';
+
+// ----------------------
+// contactページ
+} elseif (in_array($template, ['page-contact.php','page-contact-error.php','page-contact-thanks.php'])) {
+  $header_pc = get_theme_file_uri('/assets/images/pages/contact-fv-pc.jpg');
+  $header_sp = get_theme_file_uri('/assets/images/pages/contact-fv-sp.jpg');
+
+// ----------------------
+// ポリシー・規約ページ
+} elseif ($template === 'page.php') {
+  $header_pc = get_theme_file_uri('/assets/images/pages/policy-fv-pc.jpg');
+  $header_sp = get_theme_file_uri('/assets/images/pages/policy-fv-sp.jpg');
+
+  // ----------------------
+// その他ページ
+} else {
+    switch ($slug) {
+      case 'about-us':
+          $header_pc = get_theme_file_uri('/assets/images/pages/about-fv-pc.jpg');
+          $header_sp = get_theme_file_uri('/assets/images/pages/about-fv-sp.jpg');
+          break;
+      case 'information':
+          $header_pc = get_theme_file_uri('/assets/images/pages/information-fv-pc.jpg');
+          $header_sp = get_theme_file_uri('/assets/images/pages/information-fv-sp.jpg');
+          break;
+      case 'price':
+          $header_pc = get_theme_file_uri('/assets/images/pages/price-fv-pc.jpg');
+          $header_sp = get_theme_file_uri('/assets/images/pages/price-fv-sp.jpg');
+          break;
+      case 'faq':
+          $header_pc = get_theme_file_uri('/assets/images/pages/faq-fv-pc.jpg');
+          $header_sp = get_theme_file_uri('/assets/images/pages/faq-fv-sp.jpg');
+          break;
+      case 'sitemap':
+          $header_pc = get_theme_file_uri('/assets/images/pages/policy-fv-pc.jpg');
+          $header_sp = get_theme_file_uri('/assets/images/pages/policy-fv-sp.jpg');
+          break;
+      default:
+          break;
+    }
+}
+?>
+
+<div class="page-header">
+  <h1 class="page-header__title"><?php echo esc_html($header_title); ?></h1>
+  <div class="page-header__image">
       <picture>
-      <?php if (!empty($page_header_pc['url'])): ?>
-        <source srcset="<?php echo esc_url($page_header_pc['url']); ?>" media="(min-width: 768px)">
-      <?php endif; ?>
-      <img src="<?php echo esc_url($page_header_sp['url']); ?>" alt="">
+          <source srcset="<?php echo esc_url($header_pc); ?>" media="(min-width:768px)">
+          <img src="<?php echo esc_url($header_sp); ?>" alt="">
       </picture>
-    </div>
+  </div>
 
-    <!-- パンくずリスト -->
-    <div class="page-header__breadcrumb">
-      <?php get_template_part( 'template-parts/breadcrumb' ); ?>
-    </div>
-  </div> 
+  <!-- パンくずリスト -->
+  <div class="page-header__breadcrumb">
+      <?php get_template_part('template-parts/breadcrumb'); ?>
+  </div>
+</div>
